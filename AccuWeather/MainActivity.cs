086f718d516135cdeb80ba247ae1a437f326d508
Olena Bila <olena.v.bila@globallogic.com>
2018-04-16 16:48:51 +0300

    added: MainActivity empty list of sities to Main layout button to switch to SearchActivity

---
+++ AccuWeather/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Content;
+
+namespace AccuWeather
+{
+    [Activity(Label = "AccuWeather", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        public Button addCity;
+        public ListView customList;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            addCity = FindViewById<Button>(Resource.Id.addCity);
+            customList = FindViewById<ListView>(Resource.Id.customList);
+
+            addCity.Click += OnSearchClick;
+
+            customList.Adapter = new ArrayAdapter<CityWeather>(this, Android.Resource.Layout.SimpleListItem1, Android.Resource.Id.Text1 /* , + list of cities */); 
+        }
+
+        public void OnSearchClick(object sender, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(SearchActivity));
+
+            StartActivity(intent);
+        }
+
+        void OnDeleteClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            int position = e.Position;
+
+            // TODO
+        }
+    }
+}
+
---
+++ AccuWeather/Weather.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AccuWeather
+{
+    class CityWeather
+    {
+        public CityWeather(String city, String weather, String temperature)
+        {
+            CityName = city;
+            Weather = weather;
+            Temperature = temperature;
+        }
+
+        public String CityName { get; set; }
+        public String Weather { get; set; }
+        public String Temperature { get; set; }
+    }
+}
